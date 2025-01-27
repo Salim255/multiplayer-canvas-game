8c09ea8b2c9f872f841bc872318450586f01c673
Salim <salim.hassan.welleat@gmail.com>
2025-01-27 10:47:47 +0100

    project organization

---
+++ server/index.js
@@ -0,0 +1,3 @@
+// This is the file we will run with node
+require('./src/expressStuff/expressMain');
+require('./src/socketStuff/socketMain');
---
+++ server/server.js
@@ -0,0 +1,26 @@
+// Where the servers are created
+// Agar.io clone
+const { log } = require('console');
+const express = require('express');
+const socketio = require('socket.io');
+const app = express();
+
+// We define where to host public files
+app.use(express.static(__dirname+'/public'));
+
+
+const PORT = 3500;
+const expressServer = app.listen( PORT,() => {
+        console.log('Socket server run on port: ', PORT)
+})
+
+const io = socketio(expressServer);
+
+// App organization
+// server.js is NOT the entry point. it create our servers
+// and exports them 
+
+module.exports = {
+    app,
+    io
+} 
