3b7b76545fd7d234860a2bbb5f2219621fff354c
Salim <salim.hassan.welleat@gmail.com>
2025-09-08 17:11:52 +0200

    add: cors configuration

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const expressServer = app.listen( PORT,() => {
     console.log('Socket server run on port: ', PORT)
 })
 
-const io = socketio(expressServer);
+const io = socketio(expressServer, { cors: { origin: "*" } });
 
 // App organization
 // server.js is NOT the entry point. it create our servers
