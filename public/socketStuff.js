b1b08ff65f670a1b2a3a902ec0f5e717519caaf6
Salim <doshka23@gmail.com>
2025-09-08 17:15:46 +0200

    removed: port number

--- public/socketStuff.js
+++ public/socketStuff.js
@@ -1,6 +1,6 @@
 // Connect to socket server
 // Connect to the main namespace
-const socket = io.connect("https://multiplayer-canvas-game.onrender.com:3500");
+const socket = io.connect("https://multiplayer-canvas-game.onrender.com");
 
 // We will call this init function, when user
 // click on start game
